e7ad67430f4f2e3d026aa598e1ad5eb31564d2d0
NeerajTiwari17 <neeraj.tiwari.17.1@mountblue.tech>
2021-09-24 23:17:45 +0530

    add model classes, JPA repositories

---
+++ src/main/java/com/example/model/Comment.java
@@ -0,0 +1,58 @@
+package com.example.model;
+
+import java.sql.Timestamp;
+
+public class Comment {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private String comment;
+	private Integer postId;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Integer getPostId() {
+		return postId;
+	}
+	public void setPostId(Integer postId) {
+		this.postId = postId;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
---
+++ src/main/java/com/example/model/Post.java
@@ -0,0 +1,84 @@
+package com.example.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "Posts")
+public class Post {
+	
+	@Id
+	private Integer id;
+	private String title;
+	private String excerpt;
+	private String content;
+	private String author;
+	@Column(name="published_at")
+	private Timestamp publishedAt;
+	@Column(name="is_published")
+	private boolean isPublished;
+	@Column(name="created_at")
+	private Timestamp createdAt;
+	@Column(name="updated_at")
+	private Timestamp updatedAt;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getExcerpt() {
+		return excerpt;
+	}
+	public void setExcerpt(String excerpt) {
+		this.excerpt = excerpt;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Timestamp getPublishedAt() {
+		return publishedAt;
+	}
+	public void setPublishedAt(Timestamp publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+	public boolean isPublished() {
+		return isPublished;
+	}
+	public void setPublished(boolean isPublished) {
+		this.isPublished = isPublished;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}

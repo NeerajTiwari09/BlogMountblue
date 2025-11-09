cc5dbd48c6a24bafd564f48b4da1a3449a31dc0c
neerajtiwari <nt84007@gmail.com>
2025-11-09 19:11:42 +0530

    removed/commented hazelcast file, updated application.properties

--- src/main/java/com/example/Blog/config/HazelcastConfiguration.java
+++ src/main/java/com/example/Blog/config/HazelcastConfiguration.java
@@ -1,40 +1,40 @@
-package com.example.Blog.config;
-
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.cache.HazelcastCacheManager;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-@Slf4j
-public class HazelcastConfiguration {
-
-
-    @Bean
-    public ClientConfig hazelcastClientConfig() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("dev"); // Must match your Dockerized member's cluster name
-        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
-        // clientConfig.getNetworkConfig().setConnectionAttemptLimit(10);
-        // clientConfig.getNetworkConfig().setConnectionAttemptPeriod(3000);
-        // clientConfig.getNetworkConfig().setConnectionTimeout(5000);
-        return clientConfig;
-    }
-
-    @Bean
-    public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
-        return HazelcastClient.newHazelcastClient(clientConfig);
-    }
-
-    @Bean
-    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
-        return new HazelcastCacheManager(hazelcastInstance);
-    }
-
-}
+//package com.example.Blog.config;
+//
+//import com.hazelcast.client.HazelcastClient;
+//import com.hazelcast.client.config.ClientConfig;
+//import com.hazelcast.core.HazelcastInstance;
+//import com.hazelcast.spring.cache.HazelcastCacheManager;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@EnableCaching
+//@Slf4j
+//public class HazelcastConfiguration {
+//
+//
+//    @Bean
+//    public ClientConfig hazelcastClientConfig() {
+//        ClientConfig clientConfig = new ClientConfig();
+//        clientConfig.setClusterName("dev"); // Must match your Dockerized member's cluster name
+//        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
+//        // clientConfig.getNetworkConfig().setConnectionAttemptLimit(10);
+//        // clientConfig.getNetworkConfig().setConnectionAttemptPeriod(3000);
+//        // clientConfig.getNetworkConfig().setConnectionTimeout(5000);
+//        return clientConfig;
+//    }
+//
+//    @Bean
+//    public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
+//        return HazelcastClient.newHazelcastClient(clientConfig);
+//    }
+//
+//    @Bean
+//    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+//        return new HazelcastCacheManager(hazelcastInstance);
+//    }
+//
+//}
